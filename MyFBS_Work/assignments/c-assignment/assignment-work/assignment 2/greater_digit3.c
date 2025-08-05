c9e09edbd41c319707ccb29ff43044ad7318294e
Arnika03 <arnika883917@gmail.com>
2025-08-05 23:33:55 +0530

    c programs
    assignments c program

--- MyFBS_Work/assignments/c-assignment/assignment-work/assignment 2/greater_digit3.c
+++ MyFBS_Work/assignments/c-assignment/assignment-work/assignment 2/greater_digit3.c
@@ -13,16 +13,19 @@ void main(){
 	
 	if(num1>num2){
 		if(num1>num3){
-		printf("num1 is greater= %d");
+		printf("num1 is greater= %d",num1);
 	}
 	else{
-			printf("num3 is greater= %d");
+			printf("num3 is greater= %d",num3);
 	}
 }
 	else{
 		if(num2>num3){
-				printf("num2 is greater= %d");
+				printf("num2 is greater= %d",num2);
 		}
+		else{
+			printf("num3 is greater= %d",num3);
+	}
 	}
 	
 
