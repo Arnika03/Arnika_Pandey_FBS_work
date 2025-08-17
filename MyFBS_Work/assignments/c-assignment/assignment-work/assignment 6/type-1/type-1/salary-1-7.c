e99c964a0260f4358002d150df3f146d730adec4
Arnika03 <arnika883917@gmail.com>
2025-08-17 13:39:45 +0530

    function type first
    function related program

---
+++ MyFBS_Work/assignments/c-assignment/assignment-work/assignment 6/type-1/type-1/salary-1-7.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void salary();//fun decl
+void main(){
+	salary(); // fun call
+	
+}
+
+	void salary() //fun def
+
+{
+
+	double basics = 4000;
+	double da,ta,hra,ts;
+	
+	if(basics<=5000){
+		da = basics*0.10;
+		ta = basics*0.20;
+		hra = basics*0.25;
+		
+		printf("da= %lf ta= %lf hra= %lf",da,ta,hra);
+	}
+	else{
+		da = basics*0.15;
+		ta = basics*0.25;
+		hra = basics*0.30;
+		
+		printf("da= %lf ta= %lf hra= %lf   ",da,ta,hra);
+		
+		
+	}
+	ts = basics+da+ta+hra;
+	printf(" ts= %lf",ts);
+	
+}
