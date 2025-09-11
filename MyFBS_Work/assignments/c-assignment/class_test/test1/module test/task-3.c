49fe6a31461d25871c2baf8f1db2993088dcc487
Arnika03 <arnika883917@gmail.com>
2025-09-11 20:30:36 +0530

    module test
    implement a structure to store task details

---
+++ MyFBS_Work/assignments/c-assignment/class_test/test1/module test/task-3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+typedef struct Task
+{
+	int  taskid;
+    char description[100];
+    char status[30];
+	
+}Task;
+
+ addTask();
+ updateTask();
+ displayTask();
+
+int main()
+{
+	
+}
+ 
